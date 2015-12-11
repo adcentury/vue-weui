9e048bf400c484281182ae48318783a11c89948f
adcentury <adcentury100@gmail.com>
2015-12-11 14:20:26 +0800

    fix typo

--- components/toast/toast.vue
+++ components/toast/toast.vue
@@ -2,7 +2,7 @@
 <div :class="{'weui_loading_toast': type === 'loading'}">
   <div class="weui_mask_transparent"></div>
   <div class="weui_toast">
-    <div class="weui_loading" v-if="type === loading">
+    <div class="weui_loading" v-if="type === 'loading'">
       <div v-for="n in 12" class="weui_loading_leaf" :class="'weui_loading_leaf_' + n"></div>
     </div>
     <i class="weui_icon_toast" v-else></i>
