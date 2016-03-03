8bf6e1a33f2510aa34b6b6b8c77bdd14ae173194
adcentury <adcentury100@gmail.com>
2016-03-03 16:03:37 +0800

    navbar components and examples

---
+++ examples/containers/tab/tab.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="page tab">
+  <div class="bd">
+    <cells-title>Tab</cells-title>
+    <cells type="access">
+      <link-cell router-link="/navbar">
+        <span slot="body">navbar</span>
+      </link-cell>
+    </cells>
+  </div>
+</div>
+</template>
+
+<script>
+import {CellsTitle, Cells, LinkCell} from 'vue-weui';
+
+export default {
+  components: {
+    CellsTitle,
+    Cells,
+    LinkCell
+  }
+}
+</script>
