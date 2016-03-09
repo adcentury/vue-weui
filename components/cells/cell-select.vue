4df01ed35d5353423899ea69c552cd4318f37cf1
liu7923032 <1137411088@qq.com>
2016-03-09 14:42:01 +0800

    add cell-select suport object

--- components/cells/cell-select.vue
+++ components/cells/cell-select.vue
@@ -1,6 +1,7 @@
 <template>
 <select class="weui_select" v-model="selected">
-  <option v-for="option in options">{{option}}</option>
+  <option v-if="isObj" v-for="option in options" value="option.value">{{option.text}}</option>
+  <option v-else v-for="option in options" value="option">{{option}}</option>
 </select>
 </template>
 
@@ -16,6 +17,24 @@ export default {
       type: Array,
       required: true
     }
+  },
+  /*
+    定义一个元素，判断该数组中的值是否是对象
+  */
+  data(){
+      return {
+          isObj:false
+      }
+  },
+/*
+    初始化,检查该数据是否是对象, 对象的内容是 {text:'',value:''}
+*/  
+  created(){
+      var tempValue=this.options.length>0?this.options[0];
+      if(typeof tempValue=="object"){
+          this.isObj=true;
+      }
   }
+  
 }
 </script>
