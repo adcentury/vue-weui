b03139bbcfdb80040b804e6d619b3cc7929e864b
adcentury <adcentury100@gmail.com>
2016-04-14 14:43:17 +0800

    WIP

---
+++ components/mediabox/media-box.vue
@@ -0,0 +1,31 @@
+<template>
+<a href="link || 'javascript:;'" v-link="routerLink" class="weui_media_box" :class="typeClass" v-if="!!link || !!routerLink">
+  <slot></slot>
+</a>
+<div class="weui_media_box" :class="typeClass" v-else>
+  <slot></slot>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    /**
+     * 底部跳转链接
+     */
+    link: {
+      type: String,
+      required: false
+    },
+
+    /**
+     * vue-router使用的跳转链接
+     * 若使用vue-router，推荐使用router-link而非link
+     */
+    routerLink: {
+      type: null,
+      required: false
+    }
+  }
+}
+</script>
---
+++ components/panel/panel.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="weui_panel" :class="{'weui_panel_access': access}">
+  <div class="weui_panel_hd"><slot name="header"></slot></div>
+  <div class="weui_panel_bd"><slot name="body"></slot></div>
+  <a href="link || 'javascript:;'" v-link="routerLink" class="weui_panel_ft" v-if="access"><slot name="footer"></slot></a>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    /**
+     * 底部部分是否包含“查看更多”样式
+     */
+    access: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+
+    /**
+     * 底部跳转链接
+     */
+    link: {
+      type: String,
+      required: false
+    },
+
+    /**
+     * vue-router使用的跳转链接
+     * 若使用vue-router，推荐使用router-link而非link
+     */
+    routerLink: {
+      type: null,
+      required: false
+    }
+  }
+}
+</script>
